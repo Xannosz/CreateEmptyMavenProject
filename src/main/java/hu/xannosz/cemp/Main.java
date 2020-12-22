34e60f63868581385d6cd434eaf99b1111b7e370
xannosz <joli.neni@freemail.hu>
2020-12-22 03:44:03 +0100

    Create structure

--- src/main/java/hu/xannosz/cemp/Main.java
+++ src/main/java/hu/xannosz/cemp/Main.java
@@ -1,99 +1,144 @@
 package hu.xannosz.cemp;
 
-import java.awt.GridLayout;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextArea;
-import javax.swing.WindowConstants;
+import java.util.Objects;
 
 public class Main implements ActionListener {
 
-	public static JTextArea artifactId;
-	public static JTextArea name;
-	public static JComboBox<String> comboBox;
+    public static final String MICRO_TOOLS_VERSION = "0.5.2";
+    public static final String LOMBOK_VERSION = "1.12.2";
 
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("CEMP");
-		JLabel projectFolder = new JLabel("Project folder:");
-		JLabel packageName = new JLabel("Package name:");
-		JLabel projectName = new JLabel("Project name:");
-		List<String> projects = new ArrayList<>();
+    public static JTextArea artifactId;
+    public static JTextArea name;
+    public static JComboBox<String> comboBox;
 
-		File dir = new File("..");
-		for (String project : dir.list()) {
-			boolean empty = true;
-			try{
-			for (String name : new File("../" + project).list()) {
-				if (name.equals("pom.xml")) {
-					empty = false;
-				}
-			}}catch (Exception e){
-				System.out.println(e);
-			}
-			if (empty) {
-				projects.add(project);
-			}
-		}
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("CEMP");
+        JLabel projectFolder = new JLabel("Project folder:");
+        JLabel packageName = new JLabel("Package name:");
+        JLabel projectName = new JLabel("Project name:");
+        List<String> projects = new ArrayList<>();
 
-		System.out.println(projects);
+        File dir = new File("..");
+        for (String project : Objects.requireNonNull(dir.list())) {
+            boolean empty = true;
+            try {
+                for (String name : Objects.requireNonNull(new File("../" + project).list())) {
+                    if (name.equals("pom.xml")) {
+                        empty = false;
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (empty) {
+                projects.add(project);
+            }
+        }
 
-		artifactId = new JTextArea();
-		name = new JTextArea();
-		comboBox = new JComboBox<String>(projects.toArray(new String[0]));
-		JButton create = new JButton("Create");
-		create.addActionListener(new Main());
-		frame.setLayout(new GridLayout(4, 2));
-		frame.add(projectFolder);
-		frame.add(comboBox);
-		frame.add(packageName);
-		frame.add(artifactId);
-		frame.add(projectName);
-		frame.add(name);
-		frame.add(create);
-		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		frame.setSize(300, 150);
-		frame.setVisible(true);
-	}
+        System.out.println(projects);
 
-	@Override
-	public void actionPerformed(ActionEvent a) {
-		PrintWriter writer = null;
-		try {
-			writer = new PrintWriter("../" + comboBox.getSelectedItem().toString() + "/pom.xml", "UTF-8");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        artifactId = new JTextArea();
+        name = new JTextArea();
+        comboBox = new JComboBox<>(projects.toArray(new String[0]));
+        JButton create = new JButton("Create");
+        JButton update = new JButton("Update");
+        create.addActionListener(new Main());
+        update.addActionListener(new Main());
+        frame.setLayout(new GridLayout(4, 2));
+        frame.add(projectFolder);
+        frame.add(comboBox);
+        frame.add(packageName);
+        frame.add(artifactId);
+        frame.add(projectName);
+        frame.add(name);
+        frame.add(create);
+        frame.add(update);
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        frame.setSize(300, 150);
+        frame.setVisible(true);
+    }
 
-		try {
-			for (String line : Files.readAllLines(Paths.get("template.xml"))) {
-				writer.println(replaceTemplate(line));
-			}
-		} catch (IOException e) {
-		}
+    @Override
+    public void actionPerformed(ActionEvent a) {
+        if (a.getActionCommand().equals("Update")) {
+            artifactId.setText(camelToSnake((String) Objects.requireNonNull(comboBox.getSelectedItem())));
+            name.setText(camelToSpace((String) Objects.requireNonNull(comboBox.getSelectedItem())));
+        }
+        if (a.getActionCommand().equals("Create")) {
+            new File("../" + Objects.requireNonNull(comboBox.getSelectedItem()).toString() + "/src").mkdir();
 
-		writer.close();
-	}
+            new File("../" + Objects.requireNonNull(comboBox.getSelectedItem()).toString() + "/src/main").mkdir();
+            new File("../" + Objects.requireNonNull(comboBox.getSelectedItem()).toString() + "/src/test").mkdir();
 
-	private String replaceTemplate(String line) {
-		String result = line;
+            new File("../" + Objects.requireNonNull(comboBox.getSelectedItem()).toString() + "/src/main/java/"
+                    + artifactId.getText().trim().replace(".", "/")).mkdirs();
+            new File("../" + Objects.requireNonNull(comboBox.getSelectedItem()).toString() + "/src/main/resources/").mkdirs();
+            new File("../" + Objects.requireNonNull(comboBox.getSelectedItem()).toString() + "/src/test/java/"
+                    + artifactId.getText().trim().replace(".", "/")).mkdirs();
+            new File("../" + Objects.requireNonNull(comboBox.getSelectedItem()).toString() + "/src/test/resources/").mkdirs();
 
-		result = result.replaceAll("##name##", name.getText().trim());
-		result = result.replaceAll("##artifactId##", artifactId.getText().trim());
+            try (PrintWriter writer = new PrintWriter("../" + Objects.requireNonNull(comboBox.getSelectedItem()).toString() + "/pom.xml", "UTF-8")) {
+                for (String line : Files.readAllLines(new File("template.xml").toPath())) {
+                    writer.println(replaceTemplate(line));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-		System.out.println(result);
+            System.out.println("Created");
+        }
+    }
 
-		return result;
-	}
+    public static String camelToSnake(String str) {
+        StringBuilder result = new StringBuilder();
+        result.append(Character.toLowerCase(str.charAt(0)));
+
+        for (int i = 1; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (Character.isUpperCase(ch)) {
+                result.append('.');
+                result.append(Character.toLowerCase(ch));
+            } else {
+                result.append(ch);
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static String camelToSpace(String str) {
+        StringBuilder result = new StringBuilder();
+        result.append(str.charAt(0));
+
+        for (int i = 1; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (Character.isUpperCase(ch)) {
+                result.append(' ');
+                result.append(Character.toLowerCase(ch));
+            } else {
+                result.append(ch);
+            }
+        }
+
+        return result.toString();
+    }
+
+    private String replaceTemplate(String line) {
+        String result = line;
+        result = result.replaceAll("##name##", name.getText().trim());
+        result = result.replaceAll("##artifactId##", artifactId.getText().trim());
+        result = result.replaceAll("##lombokVersion##", LOMBOK_VERSION);
+        result = result.replaceAll("##microtoolsVersion##", MICRO_TOOLS_VERSION);
+        return result;
+    }
 }
